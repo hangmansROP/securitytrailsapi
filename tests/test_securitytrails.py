72d074ad5a318e2ed2778f569b0ccc767ee4aaad
Dan Duffy <de4db1t@gmail.com>
2019-07-05 23:28:24 +0100

    Added _return_error test and modified existing ping tests.

--- tests/test_securitytrails.py
+++ tests/test_securitytrails.py
@@ -2,9 +2,26 @@ import os
 import unittest
 import securitytrails
 
+
 class SecurityTrailsTests(unittest.TestCase):
-	def setUp(self):
-		self.apikey = os.environ('API_KEY')
-		self.api = securitytrails.SecurityTrailsAPI(self.apikey)
-	def test_ping_with_correct_api_key(self):
-		self.assertEqual(self.api.ping(), 200)
+    def setUp(self):
+        self.apikey = os.environ['API_KEY']
+        self.valid_api = securitytrails.SecurityTrailsAPI(self.apikey)
+        self.invalid_api = securitytrails.SecurityTrailsAPI("INVALID")
+
+    def test_ping_exception_incorrect_api_key(self):
+        with self.assertRaises(Exception):
+            self.invalid_api.ping()
+
+    def test_ping_response_content_equals_success(self):
+        ping_response = self.valid_api.ping()
+        self.assertTrue(ping_response['success'])
+
+    def test_return_error_message_values(self):
+        print(self.valid_api._return_error(401))
+        self.assertEqual(self.valid_api._return_error(401),
+                         "Invalid SecurityTrails API key")
+        self.assertEqual(self.valid_api._return_error(429),
+                         "Too many requests. Wait and try again.")
+        self.assertEqual(self.valid_api._return_error(500),
+                         "An internal error occured.")
